4975e69b0befb60836427ba176f0edb9202d29e2
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2017-11-25 17:15:49 +0300

    remove secret key from settings.py

--- digitsClassifier/settings.py
+++ digitsClassifier/settings.py
@@ -19,10 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3*l-&e3-(0=p+n=#m)bc!fgkm%av(5u=6*^)+dhpfpty-xyaez'
+with open('secret_key.txt', 'r') as f:
+    SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
