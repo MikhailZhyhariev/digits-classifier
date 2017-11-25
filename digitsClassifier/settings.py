c26e388ac07d226c03deb903b06b64f354b30f50
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2017-11-25 21:49:45 +0300

    f****ng frontend

--- digitsClassifier/settings.py
+++ digitsClassifier/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = [
     os.path.join(TEMPLATE_PATH, 'static'),
